d1d28380b6c5786a1c987ba6be386f1a2908d32e
= <connermurphy03@gmail.com>
2025-04-14 14:00:13 -0400

    MAUI app conforms to ViewModel

--- Maui.eCommerce/MainPage.xaml.cs
+++ Maui.eCommerce/MainPage.xaml.cs
@@ -1,12 +1,16 @@
-﻿namespace Maui.eCommerce;
+﻿using Maui.eCommerce.ViewModels;
+
+namespace Maui.eCommerce;
 
 public partial class MainPage : ContentPage
 {
     public MainPage()
     {
         InitializeComponent();
+        BindingContext = new MainViewModel();
     }
-    
+
+
     private void InventoryClicked(object? sender, EventArgs e)
     {
         Shell.Current.GoToAsync("//InventoryManagement"); //routing, follows singleton
---
+++ Maui.eCommerce/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+namespace Maui.eCommerce.ViewModels;
+
+public class MainViewModel
+{
+    public string Display   //binding
+    {
+        get
+        {
+            return "Hello, World!"; 
+        }
+    }
+}
